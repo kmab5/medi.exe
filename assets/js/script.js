538396774d0cac8156c62d19f15f1f606d1b6c31
Samuel Worash Getaneh <samuelworash@gmail.com>
2025-06-20 14:13:29 +0300

    added keyboard control and animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const boxes = document.querySelectorAll(".box");
     const darkToggle = document.querySelector(".dark-toggle");
     const rainbow = document.querySelectorAll(".rainbow");
+    const btns = document.querySelectorAll(".btn");
+    const indicator = document.querySelector(".indicator");
+    let vid_number = 1;
     let rainbowCtr = 0;
     let speed = 500;
     let delay = -200;
@@ -33,6 +36,37 @@ document.addEventListener("DOMContentLoaded", () => {
             });
             box.classList.toggle("active");
         });
+    });
+
+    btns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            if (btn.classList.contains("rgt")) {
+                console.log("right");
+                indicator.classList.remove(`ind-${vid_number++}`);
+                if (vid_number > 3) vid_number = 1;
+                indicator.classList.add(`ind-${vid_number}`);
+            } else if (btn.classList.contains("lft")) {
+                console.log("left");
+                indicator.classList.remove(`ind-${vid_number--}`);
+                if (vid_number < 1) vid_number = 3;
+                indicator.classList.add(`ind-${vid_number}`);
+            }
+        });
+    });
+
+    document.addEventListener("keydown", (e) => {
+        if (e.repeat) return;
+        if (e.key === "ArrowRight") {
+            console.log("right");
+            indicator.classList.remove(`ind-${vid_number++}`);
+            if (vid_number > 3) vid_number = 1;
+            indicator.classList.add(`ind-${vid_number}`);
+        } else if (e.key === "ArrowLeft") {
+            console.log("left");
+            indicator.classList.remove(`ind-${vid_number--}`);
+            if (vid_number < 1) vid_number = 3;
+            indicator.classList.add(`ind-${vid_number}`);
+        }
     });
 
     iteraterainbow();
